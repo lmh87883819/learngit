c6975a9db16bdab06fd530ba3a35510cd9d06425
lmh <1612785197@qq.com>
2018-07-17 22:12:41 +0800

    flutter第二天 ，Dart语法有待加强

--- myfirstflutterapp/lib/main.dart
+++ myfirstflutterapp/lib/main.dart
@@ -1,109 +1,181 @@
+// import 'package:flutter/material.dart';
+
+// class MyAppBar extends StatelessWidget {
+//   MyAppBar({this.title});
+
+//   // Widget子类中的字段往往都会定义为"final"
+
+//   final Widget title;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Container(
+//       height: 56.0, // 单位是逻辑上的像素（并非真实的像素，类似于浏览器中的像素）
+//       padding: const EdgeInsets.symmetric(horizontal: 8.0),
+//       decoration: new BoxDecoration(color: Colors.blue[500]),
+//       // Row 是水平方向的线性布局（linear layout）
+//       child: new Row(
+//         //列表项的类型是 <Widget>
+//         children: <Widget>[
+//           new IconButton(
+//             icon: new Icon(Icons.menu),
+//             tooltip: 'Navigation menu',
+//             onPressed: null, // null 会禁用 button
+//           ),
+//           // Expanded expands its child to fill the available space.
+//           new Expanded(
+//             flex:1,
+//             child: title,
+//           ),
+//           new IconButton(
+//             icon: new Icon(Icons.search),
+//             tooltip: 'Search',
+//             onPressed: null,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// class MyScaffold extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     // Material 是UI呈现的“一张纸”
+//     return new Material(
+//       // Column is 垂直方向的线性布局.
+//       child: new Column(
+//         children: <Widget>[
+//           new MyAppBar(
+//             title: new Text(
+//               'My title',
+//               style: Theme.of(context).primaryTextTheme.title,
+//             ),
+//           ),
+//           new Expanded(
+//             child: new Center(
+//               child: new Text('Hello, world!'),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// void main() {
+//   runApp(new MaterialApp(
+//     title: 'My app', // used by the OS task switcher
+//     home: new MyScaffold(),
+//   ));
+// }
+
+// import 'package:flutter/material.dart';
+// import 'package:myfirstflutterapp/myButton.dart';
+
+// class TutorialHome extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     //Scaffold是Material中主要的布局组件.
+//     return new Scaffold(
+//       appBar: new AppBar(
+//         leading: new IconButton(
+//           icon: new Icon(Icons.menu),
+//           tooltip: 'Navigation menu',
+//           onPressed: null,
+//         ),
+//         title: new Text('Flutter Tutorial'),
+//         actions: <Widget>[
+//           new IconButton(
+//             icon: new Icon(Icons.search),
+//             tooltip: 'Search',
+//             onPressed: null,
+//           ),
+//         ],
+//       ),
+//       //body占屏幕的大部分
+//       body: new Center(
+//         child: new Text('Hello, world!'),
+//       ),
+//       floatingActionButton: new FloatingActionButton(
+//         tooltip: 'Add', // used by assistive technologies
+//         child: new Icon(Icons.add),
+//         onPressed: null,
+//       ),
+//     );
+//   }
+// }
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(new MaterialApp(
+//     title: 'My app', // used by the OS task switcher
+//     home: new Counter(),
+//   ));
+// }
+
+// class CounterDisplay extends StatelessWidget {
+//   CounterDisplay({this.count});
+
+//   final int count;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Text('Count: $count');
+//   }
+// }
+
+// class CounterIncrementor extends StatelessWidget {
+//   CounterIncrementor({this.onPressed});
+
+//   final VoidCallback onPressed;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return new RaisedButton(
+//       onPressed: onPressed,
+//       child: new Text('Increment'),
+//     );
+//   }
+// }
+
+// class Counter extends StatefulWidget {
+//   @override
+//   _CounterState createState() => new _CounterState();
+// }
+
+// class _CounterState extends State<Counter> {
+//   int _counter = 0;
+
+//   void _increment() {
+//     setState(() {
+//       ++_counter;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Row(children: <Widget>[
+//       new CounterIncrementor(onPressed: _increment),
+//       new CounterDisplay(count: _counter),
+//     ]);
+//   }
+// }
+
 import 'package:flutter/material.dart';
+import 'package:myfirstflutterapp/shoppingList.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(new MaterialApp(
+    title: 'Shopping App',
+    home: new ShoppingList(
+      products: <Product>[
+        new Product(name: 'Eggs'),
+        new Product(name: 'Flour'),
+        new Product(name: 'Chocolate chips'),
+      ],
+    ),
+  ));
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have clicked the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
